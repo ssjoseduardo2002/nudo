75a36d676990764302b25995d38ca4f22636dccc
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-03-27 15:14:37 -0600

    barra-lateral.html barra-lateral.scss barra-lateral.ts

--- src/app/componentes_de_estructura/barra-lateral/barra-lateral.ts
+++ src/app/componentes_de_estructura/barra-lateral/barra-lateral.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-barra-lateral',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './barra-lateral.html',
   styleUrl: './barra-lateral.scss',
 })
