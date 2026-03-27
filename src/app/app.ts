2236cc2b250e077550bb98d925bc43b876651b64
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-03-27 15:22:43 -0600

    app.html app.ts modificaciones

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BarraLateral } from "./componentes_de_estructura/barra-lateral/barra-lateral";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BarraLateral],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
