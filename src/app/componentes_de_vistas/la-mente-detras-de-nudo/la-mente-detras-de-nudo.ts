03b7a164bd304b4bd11fa43f5599fba2a8c84b82
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-03-30 15:22:52 -0600

    la-mente-detras-de-nudo

--- src/app/componentes_de_vistas/la-mente-detras-de-nudo/la-mente-detras-de-nudo.ts
+++ src/app/componentes_de_vistas/la-mente-detras-de-nudo/la-mente-detras-de-nudo.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-la-mente-detras-de-nudo',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './la-mente-detras-de-nudo.html',
-  styleUrl: './la-mente-detras-de-nudo.scss',
+  styleUrl: './la-mente-detras-de-nudo.scss'
 })
-export class LaMenteDetrasDeNUDO {
-
+export class LaMenteDetrasDeNudo {
+  // Aquí puedes agregar más datos sobre ti si quieres en el futuro,
+  // como links a tus redes o portafolio.
 }
