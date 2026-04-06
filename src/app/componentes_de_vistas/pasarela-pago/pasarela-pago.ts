624070caa89cf1c5f1edcea78f2934fcd90b45a5
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-04-06 12:46:59 -0600

    Corrigiendo errores

--- src/app/componentes_de_vistas/pasarela-pago/pasarela-pago.ts
+++ src/app/componentes_de_vistas/pasarela-pago/pasarela-pago.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './pasarela-pago.html', // Usa dos puntos si el archivo físico los tiene
-  styleUrl: './pasarela-pago..scss',   // Usa dos puntos si el archivo físico los tiene
+  styleUrl: './pasarela-pago.scss',   // Usa dos puntos si el archivo físico los tiene
 })
 export class PasarelaPagoComponent { } // Revisa que el nombre sea exacto
