c3c3929d98b760b8400db11b79fff675ae0265a2
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-04-13 14:53:40 -0600

    modificaciones más

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // Esto conecta tu archivo de rutas con la aplicación
-    provideRouter(routes, withComponentInputBinding())
+    provideRouter(routes)
   ]
 };
