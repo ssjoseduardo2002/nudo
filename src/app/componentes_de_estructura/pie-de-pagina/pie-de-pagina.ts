586c4930e91835ede9781bb077ecaa1a3cb1d1aa
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-04-16 12:35:08 -0600

    pie-de-pagina html ts scss

--- src/app/componentes_de_estructura/pie-de-pagina/pie-de-pagina.ts
+++ src/app/componentes_de_estructura/pie-de-pagina/pie-de-pagina.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // VITAL para los routerLink
 
 @Component({
   selector: 'app-pie-de-pagina',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    RouterModule // Agregamos esto para que el HTML reconozca la navegación
+  ],
   templateUrl: './pie-de-pagina.html',
   styleUrl: './pie-de-pagina.scss'
 })
 export class PieDePagina implements OnInit {
+  
+  // Esta propiedad permite que el componente padre le diga al pie si debe ocultarse
+  @Input() enPantallaAcceso: boolean = false; 
+  
   anioActual: number = 0;
 
   ngOnInit(): void {
-    // Esto captura el año actual del sistema automáticamente
+    // Captura el año actual para que el copyright siempre esté al día
     this.anioActual = new Date().getFullYear();
   }
 }
