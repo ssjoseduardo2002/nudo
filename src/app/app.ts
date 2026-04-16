ee71c0128c071ba51bb1a7487cecfa2b93d4f458
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-04-16 13:49:32 -0600

    centro-ayuda

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,41 +3,32 @@ import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common'; 
 import { filter } from 'rxjs/operators';
 
-// --- COMPONENTES DE ESTRUCTURA ---
+// --- ESTRUCTURA ---
 import { Encabezado } from './componentes_de_estructura/encabezado/encabezado'; 
 import { PieDePagina } from './componentes_de_estructura/pie-de-pagina/pie-de-pagina'; 
 import { BarraLateralComponent } from './componentes_de_estructura/barra-lateral/barra-lateral'; 
 
-// --- TODAS LAS VISTAS DE NUDO ---
+// --- VISTAS ---
 import { AccesoComponent } from './componentes_de_vistas/acceso/acceso';
 import { InicioComponent } from './componentes_de_vistas/inicio/inicio';
 import { CatalogoComponent } from './componentes_de_vistas/catalogo/catalogo';
 import { GeneradorContratoComponent } from './componentes_de_vistas/generador-contrato/generador-contrato';
 import { PerfilUsuarioComponent } from './componentes_de_vistas/perfil-de-usuario/perfil-de-usuario';
 import { PasarelaPagoComponent } from './componentes_de_vistas/pasarela-pago/pasarela-pago';
+import { CentroAyudaComponent } from './componentes_de_vistas/centro-ayuda/centro-ayuda';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, 
-    CommonModule,      
-    Encabezado,       
-    PieDePagina,      
-    BarraLateralComponent,
-    // Registramos todas las vistas para que el HTML las reconozca
-    AccesoComponent,
-    InicioComponent,
-    CatalogoComponent,
-    GeneradorContratoComponent,
-    PerfilUsuarioComponent,
-    PasarelaPagoComponent
+    RouterOutlet, CommonModule, Encabezado, PieDePagina, BarraLateralComponent,
+    AccesoComponent, InicioComponent, CatalogoComponent, GeneradorContratoComponent,
+    PerfilUsuarioComponent, PasarelaPagoComponent, CentroAyudaComponent
   ], 
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class AppComponent implements OnInit {
-  title = 'NUDO';
   public mostrarSoloAcceso: boolean = true; 
 
   constructor(private router: Router) {}
@@ -47,11 +38,7 @@ export class AppComponent implements OnInit {
       filter(event => event instanceof NavigationEnd)
     ).subscribe((event: any) => {
       const urlActual = event.urlAfterRedirects || event.url;
-      // Definimos rutas de "Aterrizaje"
-      const rutasDeAcceso = ['/acceso', '/', ''];
-      this.mostrarSoloAcceso = rutasDeAcceso.includes(urlActual);
-      
-      // Aseguramos que el socio siempre empiece arriba al cambiar de vista
+      this.mostrarSoloAcceso = ['/acceso', '/', ''].includes(urlActual);
       window.scrollTo(0, 0);
     });
   }
--- src/app/componentes_de_vistas/centro-ayuda/centro-ayuda.ts
+++ src/app/componentes_de_vistas/centro-ayuda/centro-ayuda.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-centro-ayuda',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './centro-ayuda.html',
-  styleUrl: './centro-ayuda.scss',
+  styleUrl: './centro-ayuda.scss'
 })
 export class CentroAyudaComponent {
-
+  constructor() {}
 }
