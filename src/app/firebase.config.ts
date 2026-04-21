f367a628b2947a80e7c3ea0bd4cddf20f2c4a66c
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-04-21 14:14:53 -0600

    firebase.config.ts

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAa3IMbT_heAMJrBHj9GnT-U1A969tRqvA",
+  authDomain: "nudo-contratos.firebaseapp.com",
+  projectId: "nudo-contratos",
+  storageBucket: "nudo-contratos.firebasestorage.app",
+  messagingSenderId: "1055302143890",
+  appId: "1:1055302143890:web:5c204354ccbf96b5917b4b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
