6cb6735f3abe58b5943ffb8317037ab8244e7cef
Jose Eduardo Serrano Sosa <serranososae@gmail.com>
2026-04-21 13:05:47 -0600

    modificación del app.routes.ts para que los componentes ya no estén encimados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { CatalogoComponent } from './componentes_de_vistas/catalogo/catalogo';
 import { GeneradorContratoComponent } from './componentes_de_vistas/generador-contrato/generador-contrato';
 import { EditorContratoComponent } from './componentes_de_vistas/editor-contrato/editor-contrato';
 import { CentroAyudaComponent } from './componentes_de_vistas/centro-ayuda/centro-ayuda';
-// CORRECCIÓN AQUÍ: Cambiado de PerfilDeUsuarioComponent a PerfilUsuarioComponent
 import { PerfilUsuarioComponent } from './componentes_de_vistas/perfil-de-usuario/perfil-de-usuario';
 import { PasarelaPagoComponent } from './componentes_de_vistas/pasarela-pago/pasarela-pago';
 import { LaMenteDetrasDeNudoComponent } from './componentes_de_vistas/la-mente-detras-de-nudo/la-mente-detras-de-nudo';
@@ -58,7 +57,6 @@ export const routes: Routes = [
   },
   { 
     path: 'perfil-usuario', 
-    // CORRECCIÓN AQUÍ: Usando el componente correcto
     component: PerfilUsuarioComponent, 
     title: 'NUDO - Mi Perfil' 
   },
